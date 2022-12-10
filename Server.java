12e9d86b258cea9b62ce16f201110b1485da7f9f
amorimcode <bruno.amorim@mblabs.com.br>
2022-12-10 16:40:15 -0300

    thread

---
+++ Server.java
@@ -0,0 +1,94 @@
+
+import java.io.*;
+import java.net.*;
+
+// Server class
+public class Server {
+    public static void main(String[] args) throws IOException {
+        // server is listening on port 5056
+        ServerSocket ss = new ServerSocket(5056);
+
+        // running infinite loop for getting
+        // client request
+        while (true) {
+            Socket s = null;
+
+            try {
+                // socket object to receive incoming client requests
+                s = ss.accept();
+
+                System.out.println("A new client is connected : " + s);
+
+                // obtaining input and out streams
+                DataInputStream dis = new DataInputStream(s.getInputStream());
+                DataOutputStream dos = new DataOutputStream(s.getOutputStream());
+
+                System.out.println("Assigning new thread for this client");
+
+                // create a new thread object
+                Thread t = new ClientHandler(s, dis, dos);
+
+                // Invoking the start() method
+                t.start();
+
+            } catch (Exception e) {
+                s.close();
+                e.printStackTrace();
+            }
+        }
+    }
+}
+
+// ClientHandler class
+class ClientHandler extends Thread {
+    private DataInputStream dis;
+    private DataOutputStream dos;
+    private Socket s;
+
+    // Constructor
+    public ClientHandler(Socket s, DataInputStream dis, DataOutputStream dos) {
+        this.s = s;
+        this.dis = dis;
+        this.dos = dos;
+    }
+
+    @Override
+    public void run() {
+        String received;
+        while (true) {
+            try {
+                // Ask user what he wants
+                dos.writeUTF("Digite sua mensagem:");
+
+                // receive the answer from client
+                received = dis.readUTF();
+
+                if (received.equals("Exit")) {
+                    System.out.println("Client " + this.s + " sends exit...");
+                    System.out.println("Closing this connection.");
+                    this.s.close();
+                    System.out.println("Connection closed");
+                    break;
+                }
+
+                if (received.equals(null))
+                    throw new Exception("Não pode ser nulo");
+
+                // escreve mensagem recebida do cliente
+                System.out.println(received);
+
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+
+        try {
+            // closing resources
+            this.dis.close();
+            this.dos.close();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
